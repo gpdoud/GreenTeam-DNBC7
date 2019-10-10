ad8e9319c2450b9a03549eff851a8c214c285125
max-student@outlook.com <nicholasbaker7010@gmail.com>
2019-10-10 15:57:45 -0400

    vendor.service.js done

---
+++ JavaScript/vendor.service.js
@@ -0,0 +1,43 @@
+const url = "http://localhost:55555/api/vendors";
+
+let VendorService = {};
+
+VendorService.list = () => {
+  return $.getJSON(url);
+};
+
+VendorService.get = (id) => {
+  return $.getJSON(`${url}/${id}`);
+};
+VendorService.insert = (vendor) => {
+  return $.ajax({
+    method: "PUT",
+    url: url,
+    data: JSON.stringify(vendor),
+    dataType: "json",
+    crossOrigin: true,
+    contentType: "application/json"
+  });
+};
+
+VendorService.delete = (vendor) => {
+  return $.ajax({
+    method: "DELETE",
+    url: (`${url}/${vendor.id}`),
+    data: JSON.stringify(vendor),
+    dataType: "json",
+    crossOrigin: true,
+    contentType: "application/json"
+  });
+};
+
+VendorService.update = (vendor) => {
+  return $.ajax({
+    method: "PUT",
+    url: (`${url}/${vendor.id}`),
+    data: JSON.stringify(vendor),
+    dataType: "json",
+    crossOrigin: true,
+    contentType: "application/json"
+  });
+};
