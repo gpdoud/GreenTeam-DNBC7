370c582f1678ba6a0b0820cb44fecabadc30c734
max-student@outlook.com <nicholasbaker7010@gmail.com>
2019-10-10 16:24:49 -0400

    listupdated

---
+++ JavaScript/vendorlist.component.js
@@ -0,0 +1,25 @@
+const refresh = (vendors) => {
+    var tbodyCtrl = document.getElementById("tbody");
+    tbodyCtrl.innerHTML = " ";
+    for (var vendor of vendors) {
+       
+        
+        var tr = "<tr>";
+        tr += `<td>${vendor.id}</td>`
+        tr += `<td>${vendor.code}</td>`
+        tr += `<td>${vendor.name}</td>`
+        tr += `<td>${vendor.address}</td>`
+        tr += `<td>${vendor.city}</td>`
+        tr += `<td>${vendor.state}</td>`
+        tr += `<td>${vendor.zip}</td>`
+        tr += `<td>${vendor.phone}</td>`
+        tr += `<td>${vendor.email}</td>`
+        tr += "</tr>";
+    tbodyCtrl.innerHTML += tr;
+    }
+}
+
+const getVendors = () => {
+    UserService.list()
+      .done( (res) => refresh(res))
+  }
