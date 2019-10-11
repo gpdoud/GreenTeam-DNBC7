e4823e5b36ff5a29e50a49095bdfee4e943497bc
max-student@outlook.com <nicholasbaker7010@gmail.com>
2019-10-11 10:47:28 -0400

    Added VendorEdit

---
+++ JavaScript/vendoredit.component.js
@@ -0,0 +1,42 @@
+$().ready(() => {
+    $("#btnGet").click(() => {
+      let id = $("#id").val();
+      get(id);
+    });
+    $("#btnSave").click(() => {
+        let vendor = { 
+            id : $("#pid").val(),
+            code : $("#pcode").val(),
+            name : $("#pname").val(),
+            address : $("#paddress").val(),
+            city : $("#pcity").val(),
+            state : $("#pstate").val(),
+            zip : $("#pzip").val(),
+            phone : $("#pphone").val(),
+            email : $("#pemail").val(),
+            
+
+        }
+        console.log("Vendor:", vendor)
+        VendorService.update(vendor)
+            .done((res)=> {
+                console.log("Insert rc:", res);
+            });
+      });
+    });
+  const get = id => {
+    VendorService.get(id)
+    .done( vendor => {
+        $("#pid").val(vendor.id);
+        $("#pcode").val(vendor.code);
+        $("#pname").val(vendor.name);
+        $("#paddress").val(vendor.address);
+        $("#pcity").val(vendor.city);
+        $("#pstate").val(vendor.state);
+        $("#pzip").val(vendor.zip);
+        $("#pphone").val(vendor.phone);
+        $("#pemail").val(vendor.email);
+    });
+    
+  };
+  
